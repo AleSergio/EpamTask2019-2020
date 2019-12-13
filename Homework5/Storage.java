5feceb56ed483fe373fc21edfa0faf948020d875
AleSergio <46831993+AleSergio@users.noreply.github.com>
2019-12-13 17:02:26 +0300

    Update Storage.java

--- Homework5/Storage.java
+++ Homework5/Storage.java
@@ -1,7 +1,9 @@
 package Homework5;
 
 import Homework5.cargo.domain.Cargo;
+import Homework5.cargo.domain.CargoType;
 import Homework5.carrier.Carrier;
+import Homework5.carrier.CarrierType;
 
 public class Storage {
     private static final int ARRAY_CAPACITY = 2;
@@ -45,6 +47,96 @@ public class Storage {
         for (int i = 0; i < src.length; i++) {
             dest[i] = src[i];
         }
+    }
+
+    public static Cargo getAllCargos() {
+        for (Cargo cargo : cargos) {
+            if (cargo != null) {
+                System.out.println(cargo);
+            }
+        }
+        return null;
+    }
+
+    public static Carrier getAllCarriers() {
+        for (Carrier carrier : carriers) {
+            if (carrier != null) {
+                System.out.println(carrier);
+            }
+        }
+        return null;
+    }
+
+    public static Cargo getCargoById(long id) {
+        for (Cargo cargo : cargos) {
+            if (cargo != null && id == cargo.getId()) {
+                System.out.println(cargo);
+            }
+        }
+        return null;
+    }
+
+    public static Carrier getCarrierById(long id) {
+        for (Carrier carrier : carriers) {
+            if (carrier != null && id == carrier.getId()) {
+                System.out.println(carrier);
+            }
+        }
+        return null;
+    }
+
+    public static Cargo getCargoByName(String string) {
+        for (Cargo cargo : cargos) {
+            if (cargo != null && string.equals(cargo.getName())) {
+                System.out.println(cargo);
+            }
+        }
+        return null;
+    }
+
+    public static Carrier getCarrierByName(String string) {
+        for (Carrier carrier : carriers) {
+            if (carrier != null && string.equals(carrier.getName())) {
+                System.out.println(carrier);
+            }
+        }
+        return null;
+    }
+
+    public static Cargo getCargoByWeight(int weight) {
+        for (Cargo cargo : cargos) {
+            if (cargo != null && weight == cargo.getWeight()) {
+                System.out.println(cargo);
+            }
+        }
+        return null;
+    }
+
+    public static Carrier getCarrierByAddress(String address) {
+        for (Carrier carrier : carriers) {
+            if (carrier != null && address == carrier.getAddress()) {
+                System.out.println(carrier);
+            }
+        }
+        return null;
+    }
+
+    public static Cargo getByCargoType(CargoType cargotype) {
+        for (Cargo cargo : cargos) {
+            if (cargo != null && cargotype.equals(cargo.getCargoType())) {
+                System.out.println(cargo);
+            }
+        }
+        return null;
+    }
+
+    public static String getByCarrierType(CarrierType carrierType) {
+        for (Carrier carrier : carriers) {
+            if (carrier != null && carrierType.equals(carrier.getCarrierType())) {
+                System.out.println(carrier);
+            }
+        }
+        return null;
     }
 }
 
